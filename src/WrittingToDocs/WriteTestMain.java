72330bd2222d20b47052e3b2b6f04f9d296d2fa6
brian.silewski <brian.silewski@CL-QBB00116-26.ad.ndsu.edu>
2014-11-13 11:15:58 -0600

    deleted a dublicate method and updated values

--- src/WrittingToDocs/WriteTestMain.java
+++ src/WrittingToDocs/WriteTestMain.java
@@ -5,9 +5,9 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
- * Names: Zimmer, Hoffman, Silewski
- * Group name: The Best of The Class
- * Section# 2 
+ * Names: Zimmer, Hoffman, Silewski 
+ * Group name: The Best of The Class 
+ * Section# 2
  * Lab Exercise #6
  */
 public class WriteTestMain {
@@ -18,6 +18,7 @@ public class WriteTestMain {
     private static int size = 100;
     private static long estimatedTime = 0;
 
+    //generates random values for the Array
     public static void generate() {
         for (int i = 0; i < unsorted_Array.length; i++) {
             unsorted_Array[i] = (int) (Math.random() * 100000);
@@ -25,25 +26,13 @@ public class WriteTestMain {
         }
     }
 
+    //finds random values in the array and saves them in keys_Array
     public static void keyMaker() {
         int temp;
         for (int i = 0; i < keys_Array.length; i++) {
             temp = (int) (Math.random() * 50000);
             keys_Array[i] = unsorted_Array[temp];
         }
-    }
-
-    //used to find the keys
-    public static void sequentialSearch(int[] a) {
-        for (int i = 0; i < keys_Array.length; i++) {
-            for (int j = 0; j < a.length; j++) {
-                if (keys_Array[i] == a[j]) {
-                    break;
-                }
-            }
-
-        }
-
     }
 
     public static int seqSearch(int[] a, int b) {
@@ -56,7 +45,6 @@ public class WriteTestMain {
                 estimatedTime = System.nanoTime() - startTime;
                 return i;
                 //record i  index position
-
             }
         }
         return -1;
@@ -87,19 +75,22 @@ public class WriteTestMain {
         return -1; // if it doesn't find it.
     }
 
+    //returns run time
     public static long getTime() {
         return estimatedTime;
     }
 
     public static void main(String[] args) {
 
+        /*
+         * sets up other classes and then generates random values
+         * then sorts, keys, 
+         */
         WriteToWord w = new WriteToWord(size);
         Quicksort qsort = new Quicksort();
         generate();
         qsort.qSort(sorted_Array, 0, sorted_Array.length - 1);
         keyMaker();
-        sequentialSearch(unsorted_Array);
-        sequentialSearch(sorted_Array);
 
         for (int i = 0; i < keys_Array.length; i++) {
             w.addTo(i, 0, keys_Array[i]);
@@ -117,56 +108,18 @@ public class WriteTestMain {
              */
         }
         for (int i = 0; i < 100; i++) {
-            System.out.println(unsorted_Array[i] + "\t" + unsorted_Array[49899 + i]);
+            System.out.println(unsorted_Array[i] + "\t" + unsorted_Array[49899 + i]
+                    + "\t" + sorted_Array[i] + "\t" + sorted_Array[49899 + i]);
         }
         /* 
          prints out the first 100 and last 100 values of our unsorted array.   
          */
 
-        /*
-         int ha = 5000;
-         WriteToWord w = new WriteToWord(ha);
-
-         int[] key = new int[ha];
-
-         for (int x = 0; x < ha; x++) {
-         key[x] = x + 1;
-         }
-
-         for (int c = 0; c < ha; c++) {
-         w.addTo(c, 0, key[c]);
-
-         w.addTo(c, 1, rand());
-
-         w.addTo(c, 2, rand());
-
-         w.addTo(c, 3, rand());
-
-         w.addTo(c, 4, rand());
-
-         w.addTo(c, 5, rand());
-
-         w.addTo(c, 6, rand());
-
-         w.addTo(c, 7, rand());
-
-         w.addTo(c, 8, rand());
-         }
-
-         */
         try {
             w.WriteToMs();
         } catch (IOException ex) {
             Logger.getLogger(WriteTestMain.class.getName()).log(Level.SEVERE, null, ex);
         }
-
-        int[][] test = new int[2][2];
-        test[0][0] = 1;
-        test[0][1] = 2;
-        test[1][0] = 3;
-        test[1][1] = 4;
-        System.out.println(test[0][0] + " " + test[0][1]);
-        System.out.println(test[1][0] + " " + test[1][1]);
 
     }
 
